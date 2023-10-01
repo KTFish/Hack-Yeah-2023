819223589fa70cbe4aa4c9a4550bdbbf5c332e4d
j-brzoz <jan.b.brzozowski@gmail.com>
2023-10-01 07:24:53 +0200

    cleanup

--- dochody/clearSprawozdanie.py
+++
@@ -1,9 +0,0 @@
-import csv
-import os
-
-if __name__ == "__main__":
-    os.remove("dochody/output/sprawozdanie.csv")
-    with open('dochody/output/sprawozdanie.csv', mode='w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file)
-        header = ['Id_sprawozdania', 'Id_okresu', 'Id_jednostki', 'Id_naglowka', 'Id_pozycji']
-        csv_writer.writerow(header)
